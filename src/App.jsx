bd0565719a2ba1f253ea4eb9bdcad86f32fb05b0
CavidSamedov <semedovcavid50@gmail.com>
2024-04-25 17:31:51 +0400

    Task upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Train from './components/train'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Train />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/train.jsx
@@ -0,0 +1,66 @@
+import './train.css'
+function Train() {
+ 
+
+    return (
+      <>
+      <div class='head'>
+       <h1>Javid Samadov</h1>
+       <hr></hr>
+       <p class='phone'>Phone   +994 55 464 89 69</p>
+       <p class='mail'>E-Mail: semedovcavid50@gmail.com</p>
+       </div>
+       <h3>Education</h3>
+       <hr></hr>
+       <div class='edu'>
+       <div class='uni'>
+       <p><b>Azerbaijan State Oil and Industry University</b><br></br>
+       Bachelor of Science in Computer Engineering</p>
+       </div>
+       <div class='location'>
+        <p> Baku, Azerbaijan<br></br> 2021- current</p>
+        </div>
+        </div>
+       <div class='skill'>
+        <p><b> SKILLS</b></p><hr></hr>
+        <ul>
+            <li> Programming Languages :<b> Python, C++, MATLAB</b></li>
+            <li>Databases :<b>MySQL</b></li>
+            <li> Proficient in using Microsoft Office Suite <b>(Word, Excel, PowerPoint, Outlook)</b></li>
+            <li>Experience with <b>Windows</b> operating system</li>
+            <li>Basicunderstanding of computer networks</li>
+        </ul>
+        </div> 
+        <p><b> ACHIEVEMENTS and CERTIFICATES</b></p><hr></hr>
+        <ul>
+            <li><b>APTISCertificate- Overall CEFR Grade C</b></li>
+             <ul>
+                <li> Demonstrated proficiency in English language skills at a level equivalent to <b>B2</b> on
+ the Common European Framework of Reference for Languages (CEFR)</li>
+ <li>Scored <b>150 out of 200</b> in the Listening, Reading, Speaking, and Writing sections</li>
+             </ul>
+             <li><b> Global Sports Conference MINEPS Recognition Certificate</b></li>
+              <ul>
+                <li>Appreciated for invaluable contribution and dedication to making the conference a
+ distinct success</li>
+              </ul>
+              <li><b>Formula 1 Azerbaijan Grand Prix 2023 Appreciation Certificate</b></li>
+              <ul>
+                <li>Awarded for outstanding performance during the Formula 1 Azerbaijan Grand Prix 2023</li>
+              </ul>
+        </ul>
+        <p><b> LANGUAGES</b></p>
+        <hr></hr>
+        <ul>
+            <li><b>Azerbaijani</b>: Native speaker with experience in translating and interpreting from Azerbaijani to English
+ and Turkish.</li>
+ <li><b>English</b>: Upper-intermediate proficiency (B2 CEFR) with experience in writing and speaking English in a
+ professional setting.</li>
+ <li><b>Turkish</b>: Good proficiency with experience in understanding and speaking Turkish in everyday situations</li>
+        </ul>
+      </>
+    )
+  }
+  
+  export default Train
+  
